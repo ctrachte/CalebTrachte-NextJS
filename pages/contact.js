f40ad3b21202f2e2f367af79e8a91fd3a37b8b59
Caleb Trachte <cetrachte@hotmail.com>
2021-09-15 10:26:24 -0500

    add front-end methods, useState, and jsx to make functional form

--- pages/contact.js
+++ pages/contact.js
@@ -1,22 +1,27 @@
 
 import Head from 'next/head'
 import styles from '../styles/contact.module.css'
+import { useState } from 'react'
 
 export default function Contact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [submitted, setSubmitted] = useState(false)
     return (
         <div className={styles.container}>
             < form className={styles.main} >
                 < formGroup className={styles.inputGroup} >
                     < label htmlFor='name'>Name</label>
-                    < input type='text' name='name' className={styles.inputField} />
+                    < input type='text' onChange={(e)=>{setName(e.target.value)}} name='name' className={styles.inputField} />
                 </formGroup>
                 < formGroup className={styles.inputGroup} >
                     < label htmlFor='email'>Email</label>
-                    < input type='email' name='email' className={styles.inputField} />
+                    < input type='email' onChange={(e)=>{setEmail(e.target.value)}} name='email' className={styles.inputField} />
                 </formGroup>
                 < formGroup className={styles.inputGroup} >
                     < label htmlFor='message'>Message</label>
-                    < input type='text' name='message' className={styles.inputField} />
+                    < input type='text' onChange={(e)=>{setMessage(e.target.value)}} name='message' className={styles.inputField} />
                 </formGroup>
                 < input type='submit' />
             </form >
