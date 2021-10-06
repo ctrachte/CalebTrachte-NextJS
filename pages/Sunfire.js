3060e04b9e1799916a2bcf5c6724716d8bf00cff
Caleb Trachte <cetrachte@hotmail.com>
2021-10-06 15:53:57 -0500

    add all pages for routing

---
+++ pages/Sunfire.js
@@ -0,0 +1,15 @@
+import Footer from '../components/footer'
+import Head from 'next/head'
+import { motion } from 'framer-motion'
+
+function Sunfire() {
+  return (
+    <div className="container">
+    <Head>
+    </Head>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Sunfire
