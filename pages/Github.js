a0a508477e4aa721219ca0996b9eea0027c062c6
Caleb Trachte <cetrachte@hotmail.com>
2021-10-08 10:59:40 -0500

    add async API call to github API, adjustments to interface for that page

--- pages/Github.js
+++ pages/Github.js
@@ -3,14 +3,39 @@ import Header from '../components/header';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
 
-function Github() {
+function Github({ posts }) {
   return (
-    <div className="container">
-      <Head></Head>
-      <Header/>
-      <Footer />
-    </div>
+
+        <div className="container">
+        <Head></Head>
+        <Header/>
+        <ul>
+      {profiles.map((profile) => (
+        <li>{profile.title}</li>
+      ))}
+    </ul>
+        <Footer />
+      </div>
   )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('https://api.github.com/users/ctrachte');
+  const profiles = await res.json();
+  console.log(profiles)
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      profiles,
+    },
+  }
 }
 
 export default Github
