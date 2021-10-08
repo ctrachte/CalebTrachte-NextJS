92241721cb120c23dd80a162a4f8ff2849bba7f1
Caleb Trachte <cetrachte@hotmail.com>
2021-10-08 10:18:34 -0500

    add header to Github route

--- pages/Github.js
+++ pages/Github.js
@@ -1,11 +1,13 @@
-import Footer from '../components/footer'
-import Head from 'next/head'
-import { motion } from 'framer-motion'
+import Footer from '../components/footer';
+import Header from '../components/header';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
 
 function Github() {
   return (
     <div className="container">
       <Head></Head>
+      <Header/>
       <Footer />
     </div>
   )
