dbfdb79c1d0c32f4d64e3ea8a48239a876f056e8
Caleb Trachte <cetrachte@hotmail.com>
2021-10-08 12:56:29 -0500

    add proper dynamic assets to github card/page

--- pages/Github.js
+++ pages/Github.js
@@ -1,18 +1,40 @@
-import Footer from '../components/footer';
-import Header from '../components/header';
-import Head from 'next/head';
-import { motion } from 'framer-motion';
+import Footer from '../components/footer'
+import Header from '../components/header'
+import Head from 'next/head'
+import { motion } from 'framer-motion'
 
 function Github({ profile }) {
   return (
-      <div>
-        <Head></Head>
-        <Header/>
-            {
-              <div>{profile.bio}</div>
-            }
-        <Footer />
-      </div>
+    <div>
+      <Head></Head>
+      <Header />
+      {
+        <div>
+          <ul className="card-list">
+            <li className="card">
+              <a
+                id="githubProfilePic"
+                className="card-image"
+                href={profile.html_url}
+                target="_blank"
+              >
+                <img src={profile.avatar_url} alt="Github Avatar" />
+              </a>
+              <a
+                className="card-description"
+                href={profile.html_url}
+                target="_blank"
+              >
+                <h2>{profile.name}</h2>
+                <h3>{profile.company}</h3>
+                <p>{profile.bio}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      }
+      <Footer />
+    </div>
   )
 }
 
@@ -21,13 +43,14 @@ function Github({ profile }) {
 // direct database queries. See the "Technical details" section.
 export async function getStaticProps() {
   // Call an external API endpoint to get user profile
-  const res = await fetch('https://api.github.com/users/ctrachte');
-  const profile = await res.json();
+  const res = await fetch('https://api.github.com/users/ctrachte')
+  const profile = await res.json()
 
   return {
     props: {
       profile,
     },
+    revalidate: 10,
   }
 }
 
