d3e4ea5bf0976eefea962633372fe6956f867968
Caleb Trachte <cetrachte@hotmail.com>
2021-10-13 13:23:48 -0500

    fixed styling css react conflicts, adjustments to placement of styling and origins

--- components/moment-datepicker.js
+++ components/moment-datepicker.js
@@ -1,23 +1,30 @@
-import momentDatepicker from 'moment-datepicker-js';
+import momentDatepicker from 'moment-datepicker-js'
 //With newer versions of React you can use and manipulate the DOM via hooks like this:
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react'
 
 const Datepicker = () => {
-
-  const myContainer = useRef(null);
-  let datepicker;
+  const myContainer = useRef(null)
+  let datepicker
   useEffect(() => {
-    datepicker = new momentDatepicker({containerElement: myContainer.current})
+    datepicker = new momentDatepicker({ containerElement: myContainer.current })
     console.log(myContainer.current, datepicker)
-  });
+  })
 
   return (
     <>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
+        integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      ></script>
       <h1>Datepicker</h1>
       <div ref={myContainer}></div>
-    </>
-  );
-};
+      <style jsx>{`
 
-export default Datepicker;
+      `}</style>
+    </>
+  )
+}
+
+export default Datepicker
--- pages/Datepicker.js
+++ pages/Datepicker.js
@@ -1,12 +1,13 @@
 import Footer from '../components/footer'
 import Head from 'next/head'
 import MomentDatepicker from '../components/moment-datepicker'
+import styles from '../components/moment-datepicker.module.css'
 
 function Datepicker() {
   return (
     <div className="container">
       <Head></Head>
-      <MomentDatepicker />
+      <MomentDatepicker style={styles}/>
       <Footer />
     </div>
   )
