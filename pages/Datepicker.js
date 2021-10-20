3299bcb78247f8caedd9b3c7e9b824fa5ba8da61
Caleb Trachte <cetrachte@hotmail.com>
2021-10-20 12:43:08 -0500

    updated Datepicker page to include header

--- pages/Datepicker.js
+++ pages/Datepicker.js
@@ -1,5 +1,6 @@
 import Footer from '../components/footer'
 import Head from 'next/head'
+import Header from '../components/header'
 import MomentDatepicker from '../components/moment-datepicker'
 import styles from '../components/moment-datepicker.module.css'
 
@@ -7,6 +8,7 @@ function Datepicker() {
   return (
     <div className="container">
       <Head></Head>
+      <Header/>
       <MomentDatepicker style={styles}/>
       <Footer />
     </div>
