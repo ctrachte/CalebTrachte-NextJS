689f8f8915ae2598a69c5b5814cab1dafd68bcad
Caleb Trachte <cetrachte@hotmail.com>
2021-11-03 11:24:17 -0500

    updated datepicker component to include options array, methods scoped to the component for change  events and initialization

--- components/moment-datepicker.js
+++ components/moment-datepicker.js
@@ -3,12 +3,52 @@ import momentDatepicker from 'moment-datepicker-js'
 import React, { useEffect, useRef } from 'react'
 
 const Datepicker = () => {
-  const myContainer = useRef(null)
-  let datepicker
+
+  const myContainer = useRef(null);
+
+  let datepicker;
+
   useEffect(() => {
-    datepicker = new momentDatepicker({ containerElement: myContainer.current })
-    //    console.log(myContainer.current, datepicker)
+    // reset container and options for datepicker
+    myContainer.current.innerHTML = "";
+    let initialOptions = {
+      containerElement: myContainer.current,
+      presetMenu: true,
+      singleDate: false,
+      autoClose: false,
+      timePicker: true,
+      leadingTrailingDates: true,
+      clearDates: true,
+      defaults: false,
+      militaryTime: false,
+      // max: moment("08/14/2022"),
+      // min: new Date("08/14/2021"),
+      // defaults: [new Date('08/14/2023'),new Date('08/14/2023')],
+      menuOptions: [{ title: 'My Birthday', values: [new Date('08/14/1993'),new Date('08/14/1993')] }],
+      startDateLabel: "Reservation Start: ",
+      endDateLabel: "Reservation End: ",
+      onChange: function () {
+          console.log("onChange:", this.dates);
+      },
+      onSubmit: function () {
+          console.log("onSubmit:", this.dates);
+      },
+      onClose: function () {
+          console.log("onClose:", this.dates);
+      }
+    }
+    //initialize datepicker with options
+    datepicker = initDatepicker(initialOptions);
   })
+
+  let changeOptions = (e) => {
+    //code to get and set options from UI;
+  }; 
+
+  //method to initialize datepicker
+  function initDatepicker(options) {
+    return new momentDatepicker(options);
+  }
 
   return (
     <>
