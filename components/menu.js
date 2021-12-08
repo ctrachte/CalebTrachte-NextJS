14ef110da68ee83747411d711e98016711323129
Caleb Trachte <cetrachte@hotmail.com>
2021-12-08 11:22:19 -0600

    installed side menu, imported and implemented on Datepicker.js

---
+++ components/menu.js
@@ -0,0 +1,15 @@
+import {SideMenu} from 'react-sidemenu';
+
+
+export default function menu() {
+    return (
+      <SideMenu>
+        <ul>
+        <li>
+            Test Menu Item
+        </li>
+        </ul>
+      </SideMenu>
+    )
+  }
+  
--- pages/Datepicker.js
+++ pages/Datepicker.js
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Header from '../components/header'
 import MomentDatepicker from '../components/moment-datepicker'
 import styles from '../components/moment-datepicker.module.css'
+import Menu from '../components/menu'
 
 function Datepicker() {
   return (
     <div className="container">
       <Head></Head>
       <Header/>
+      <Menu/>
       <MomentDatepicker style={styles}/>
       <Footer />
     </div>
