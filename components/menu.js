b937cc47abb410be1402841894d9106cfca06cd8
Caleb Trachte <cetrachte@hotmail.com>
2021-12-08 16:06:04 -0600

    add onHover methods to collapse and expand menu, and add state and font awesome icons for collapsed state

--- components/menu.js
+++ components/menu.js
@@ -1,27 +1,38 @@
 import React from "react";
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHammer, faPlay } from '@fortawesome/free-solid-svg-icons'
 
 class SideMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { collapsed: true };
+    this.Open = this.Open.bind(this);
+    this.hoverMenu = this.hoverMenu.bind(this);
+    this.Close = this.Close.bind(this);
+  }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  hoverMenu(event) {
+    event.preventDefault();
+    this.state.collapsed ? this.Open() : this.Close();
   }
 
   Open() {
-    this.setState({ opened: false });
+    this.setState({ collapsed: false });
   }
   Close() {
-    this.setState({ opened: false });
+    this.setState({ collapsed: true });
   }
 
   render() {
     return (
       <div className="SideMenuContainer">
-      <ProSidebar collapsed={false}>
-        <Menu iconShape="square">
-          <MenuItem>Example Implementation</MenuItem>
-          <SubMenu title="Documentation">
+      <ProSidebar collapsed={this.state.collapsed} onMouseEnter={this.hoverMenu} onMouseLeave={this.hoverMenu} >
+        <Menu>
+          <MenuItem icon={<FontAwesomeIcon icon={faPlay} />}>Datepicker Sandbox</MenuItem>
+          <SubMenu title="Documentation" icon={<FontAwesomeIcon icon={faHammer} />}>
             <MenuItem>Initialization</MenuItem>
             <MenuItem>.Value()</MenuItem>
           </SubMenu>
