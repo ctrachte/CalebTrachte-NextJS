48068da7c9bd4b3a09564d0cd67353eeaf66e36b
Caleb Trachte <cetrachte@hotmail.com>
2022-04-06 13:01:23 -0500

    moved datepicker gif, moved styling for side menu so they will be in scope

--- components/docs/about.js
+++ components/docs/about.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import mypic from "./Datepicker_V1.0.0.gif";
 
 export default function About() {
   return (
@@ -11,7 +10,7 @@ export default function About() {
         }}
       >
         <Image
-          src={mypic}
+          src={"/Datepicker_V1.0.0.gif"}
           alt="Datepicker.js Demonstration gif"
           layout="intrinsic"
           objectPosition={"center center"}
