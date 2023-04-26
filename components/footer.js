906fe0a4073ef2c53e72471cb9357bba9e6c7adf
Caleb Trachte <cetrachte@hotmail.com>
2023-04-26 11:50:07 -0500

    updated footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div>&copy; 2022 Caleb Trachte - <small>Powered by NextJS and Vercel</small></div>
+      <div>&copy; 2023 Caleb Trachte - <small>Powered by NextJS and Vercel</small></div>
     </footer>
   )
 }
